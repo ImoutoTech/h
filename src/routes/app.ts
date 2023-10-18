a49a5de71f54226bc42bfa5c0d507312ab427c8b
youranreus <youranreus@qq.com>
2023-10-18 12:52:11 +0800

    feat: sk&子应用错误处理调整

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -17,131 +17,103 @@ const router = express.Router()
 /**
  * 子应用注册
  */
-router.post('/reg', async (req, res, next) => {
-  try {
-    if (!req.user || req.user.refresh) {
-      throw new Error('give me the token')
-    }
-
-    const { body } = req
-    const paramList = ['name', 'callback']
-
-    if (!checkParams(body, paramList)) {
-      throw new Error('missing params')
-    }
-
-    retSuccess(
-      res,
-      await RegisterApp(
-        {
-          ...body,
-          owner: req.user.id,
-        },
-        req.redis
-      )
-    )
-  } catch (e) {
-    next(e)
+router.post('/reg', async (req, res) => {
+  if (!req.user || req.user.refresh) {
+    throw new Error('give me the token')
   }
+
+  const { body } = req
+  const paramList = ['name', 'callback']
+
+  if (!checkParams(body, paramList)) {
+    throw new Error('missing params')
+  }
+
+  retSuccess(
+    res,
+    await RegisterApp(
+      {
+        ...body,
+        owner: req.user.id,
+      },
+      req.redis
+    )
+  )
 })
 
 /**
  * 获取用户子应用列表
  */
-router.get('/my', async (req, res, next) => {
-  try {
-    if (!req.user || req.user.refresh) {
-      throw new Error('give me the token')
-    }
-
-    retSuccess(res, await getUserApp(req.user.id))
-  } catch (e) {
-    next(e)
+router.get('/my', async (req, res) => {
+  if (!req.user || req.user.refresh) {
+    throw new Error('give me the token')
   }
+
+  retSuccess(res, await getUserApp(req.user.id))
 })
 
 /**
  * 获取所有子应用列表
  */
-router.get('/all', async (req, res, next) => {
-  try {
-    if (!req.user || req.user.refresh) {
-      throw new Error('give me the token')
-    }
-
-    if (req.user.role !== ROLE.ADMIN) {
-      throw new Error('not admin')
-    }
-
-    const { page, size } = req.query
-
-    retSuccess(res, await getAllApp(Number(page), Number(size)))
-  } catch (e) {
-    next(e)
+router.get('/all', async (req, res) => {
+  if (!req.user || req.user.refresh) {
+    throw new Error('give me the token')
   }
+
+  if (req.user.role !== ROLE.ADMIN) {
+    throw new Error('not admin')
+  }
+
+  const { page, size } = req.query
+
+  retSuccess(res, await getAllApp(Number(page), Number(size)))
 })
 
 /**
  * 子应用回调
  */
-router.post('/:id', async (req, res, next) => {
-  try {
-    if (!req.user || req.user.refresh) {
-      throw new Error('give me the token')
-    }
-    retSuccess(res, await callbackSubApp(req.params.id, req.user.id, req.redis))
-  } catch (e) {
-    next(e)
+router.post('/:id', async (req, res) => {
+  if (!req.user || req.user.refresh) {
+    throw new Error('give me the token')
   }
+  retSuccess(res, await callbackSubApp(req.params.id, req.user.id, req.redis))
 })
 
 /**
  * 获取子应用信息
  */
-router.get('/:id', async (req, res, next) => {
-  try {
-    retSuccess(res, await getSubAppData(req.params.id, req.redis))
-  } catch (e) {
-    next(e)
-  }
+router.get('/:id', async (req, res) => {
+  retSuccess(res, await getSubAppData(req.params.id, req.redis))
 })
 
 /**
  * 子应用删除
  */
-router.delete('/:id', async (req, res, next) => {
-  try {
-    if (!req.user || req.user.refresh) {
-      throw new Error('give me the token')
-    }
-
-    retSuccess(res, {
-      result: await delSubApp(req.params.id, req.user.id, req.redis),
-    })
-  } catch (e) {
-    next(e)
+router.delete('/:id', async (req, res) => {
+  if (!req.user || req.user.refresh) {
+    throw new Error('give me the token')
   }
+
+  retSuccess(res, {
+    result: await delSubApp(req.params.id, req.user.id, req.redis),
+  })
 })
 
 /**
  * 更新应用信息
  */
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { params, redis, body, user } = req
+router.put('/:id', async (req, res) => {
+  const { params, redis, body, user } = req
 
-    if (!params.id || !Object.keys(body).length) {
-      throw new Error('参数缺失')
-    }
-
-    if (!user?.id || user?.refresh) {
-      throw new Error('token invalid')
-    }
-
-    retSuccess(res, await ModifySubApp(body, params.id, user, redis))
-  } catch (e) {
-    next(e)
+  if (!params.id || !Object.keys(body).length) {
+    throw new Error('参数缺失')
   }
+
+  if (!user?.id || user?.refresh) {
+    throw new Error('token invalid')
+  }
+
+  retSuccess(res, await ModifySubApp(body, params.id, user, redis))
 })
 
 export default router
--- src/routes/sk.ts
+++ src/routes/sk.ts
@@ -1,37 +1,29 @@
-import express from "express";
-import { retSuccess } from "../utils/restful";
-import { getSK } from "../service/SKService";
+import express from 'express'
+import { retSuccess } from '../utils/restful'
+import { getSK } from '../service/SKService'
 
-const router = express.Router();
+const router = express.Router()
 
 /**
  * 获取用户SK
  */
-router.get("/", async (req, res, next) => {
-  try {
-    if (!req.user) {
-      throw new Error("give me the token");
-    }
-
-    retSuccess(res, await getSK(req.user.id, false));
-  } catch (e) {
-    next(e);
+router.get('/', async (req, res) => {
+  if (!req.user) {
+    throw new Error('give me the token')
   }
-});
+
+  retSuccess(res, await getSK(req.user.id, false))
+})
 
 /**
  * 更新用户SK
  */
-router.put("/", async (req, res, next) => {
-  try {
-    if (!req.user) {
-      throw new Error("give me the token");
-    }
-
-    retSuccess(res, await getSK(req.user.id, true));
-  } catch (e) {
-    next(e);
+router.put('/', async (req, res) => {
+  if (!req.user) {
+    throw new Error('give me the token')
   }
-});
 
-export default router;
+  retSuccess(res, await getSK(req.user.id, true))
+})
+
+export default router
