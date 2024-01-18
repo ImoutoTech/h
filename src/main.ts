bb4ce83bf78c7a20c914e17128f3c8d102cc99a0
youranreus <youranreus@qq.com>
2024-01-18 22:30:39 +0800

    feat: 版本控制&fastify配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1'],
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
