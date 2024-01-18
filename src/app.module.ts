cd9c68fd70b64c968e68dfd3c1671e3cc834d866
youranreus <youranreus@qq.com>
2024-01-18 23:19:09 +0800

    feat: 环境变量配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { ENV_LIST } from './utils/constants';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [...ENV_LIST],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
