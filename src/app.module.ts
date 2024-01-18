34dc6f789750b833a8168412f7afe7f64340cb14
youranreus <youranreus@qq.com>
2024-01-18 23:27:01 +0800

    feat: mysql配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ENV_LIST } from './utils/constants';
 
 @Module({
@@ -10,6 +11,20 @@ import { ENV_LIST } from './utils/constants';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [...ENV_LIST],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_SERVER', 'localhost'),
+        port: configService.get<number>('MYSQL_PORT', 3306),
+        username: configService.get('MYSQL_USER', 'root'),
+        password: configService.get('MYSQL_PASSWORD', 'root'),
+        database: configService.get('MYSQL_DATABASE', 'h'),
+        synchronize: false,
+        autoLoadEntities: true,
+      }),
     }),
   ],
   controllers: [AppController],
