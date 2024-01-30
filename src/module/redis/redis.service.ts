6d90a52c9aecfd27c9c5cd68538ed3d06ce5b8b0
季悠然 <youranreus@qq.com>
2024-01-30 23:46:27 +0800

    feat: redis日志优化

--- src/module/redis/redis.service.ts
+++ src/module/redis/redis.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Logger } from '@nestjs/common';
+import { isNil } from 'lodash';
 import { RedisClientType } from 'redis';
 import { REDIS_CLIENT } from '@/utils/constants';
 
@@ -7,8 +8,24 @@ export class RedisService {
   @Inject(REDIS_CLIENT)
   private redisClient: RedisClientType;
 
+  private logger = new Logger();
+
+  log(text: string) {
+    this.logger.log(text, 'RedisService');
+  }
+
+  warn(text: string) {
+    this.logger.warn(text, 'RedisService');
+  }
+
   async get(key: string) {
-    return await this.redisClient.get(key);
+    const data = await this.redisClient.get(key);
+    if (isNil(data)) {
+      this.warn(`Redis get key ${key}，缓存未命中`);
+    } else {
+      this.log(`Redis get key ${key}，命中缓存`);
+    }
+    return data;
   }
 
   async set(key: string, value: string | number, ttl?: number) {
@@ -17,10 +34,17 @@ export class RedisService {
     if (ttl) {
       await this.redisClient.expire(key, ttl);
     }
+    this.log(`Redis set key ${key}，更新缓存, ttl=${ttl}`);
   }
 
   async hashGet(key: string) {
-    return await this.redisClient.hGetAll(key);
+    const data = await this.redisClient.hGetAll(key);
+    if (isNil(data)) {
+      this.warn(`Redis hashGet key ${key}，缓存未命中`);
+    } else {
+      this.log(`Redis hashGet key ${key}，命中缓存`);
+    }
+    return data;
   }
 
   async hashSet(key: string, obj: Record<string, any>, ttl?: number) {
@@ -31,5 +55,6 @@ export class RedisService {
     if (ttl) {
       await this.redisClient.expire(key, ttl);
     }
+    this.log(`Redis hashSet key ${key}，更新缓存, ttl=${ttl}`);
   }
 }
